556cd1e6976f95dd168652db79dc292ee65135fc
Renaud11232 <gaspardrenaud@hotmail.com>
2016-06-15 01:21:57 +0200

    fixed tab completer

--- src/be/renaud11232/plugins/subcommands/ComplexTabCompleter.java
+++ src/be/renaud11232/plugins/subcommands/ComplexTabCompleter.java
@@ -11,18 +11,18 @@ import java.util.*;
 public class ComplexTabCompleter implements TabCompleter {
 
     private Map<String, TabCompleter> subCompleters;
-    private TabCompleter completer;
 
-    public static final TabCompleter DEFAULT_TAB_COMPLETER = new TabCompleter() {
+    public static final TabCompleter DEFAULT_COMPLETER = new TabCompleter() {
         @Override
         public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
-            List<String> completion = new LinkedList<>();
-                for(Player player : Bukkit.getOnlinePlayers()){
-                    if(strings.length == 0 || player.getName().startsWith(strings[strings.length - 1])) {
-                        completion.add(player.getName());
-                    }
+            /*List<String> completion = new LinkedList<>();
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                if (strings.length == 0 || player.getName().startsWith(strings[strings.length - 1])) {
+                    completion.add(player.getName());
                 }
-            return completion;
+            }
+            return completion;*/
+            return null;
         }
     };
     public static final TabCompleter NO_TAB_COMPLETER = new TabCompleter() {
@@ -34,17 +34,11 @@ public class ComplexTabCompleter implements TabCompleter {
 
     public ComplexTabCompleter() {
         subCompleters = new HashMap<>();
-        completer = null;
     }
 
-    public ComplexTabCompleter(TabCompleter completer, SubTab... subTabs) {
+    public ComplexTabCompleter(SubTab... subTabs) {
         this();
-        setCompleter(completer);
         addSubTabs(subTabs);
-    }
-
-    public void setCompleter(TabCompleter completer) {
-        this.completer = completer;
     }
 
     public void addSubTab(SubTab subTab) {
@@ -62,28 +56,22 @@ public class ComplexTabCompleter implements TabCompleter {
 
     @Override
     public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
-        /*List<String> completion = new LinkedList<>();
-        if (strings.length == 0) {
-            completion.addAll(subCompleters.keySet());
-        } else {
+        if (strings.length > 0) {
+            List<String> completion = new LinkedList<>();
             if (subCompleters.containsKey(strings[0])) {
-                if(strings.length > 1) {
-                    return subCompleters.get(strings[0]).onTabComplete(commandSender, command, s, Arrays.copyOfRange(strings, 1, strings.length));
-                } else {
-                    return completion;
-                }
+                return subCompleters.get(strings[0]).onTabComplete(commandSender, command, s, Arrays.copyOfRange(strings, 1, strings.length));
             } else {
-                for (String str : subCompleters.keySet()) {
-                    if (str.startsWith(strings[0])) {
-                        completion.add(str);
+                if(strings.length == 1) {
+                    for (String subCommand : subCompleters.keySet()) {
+                        if (subCommand.startsWith(strings[0])) {
+                            completion.add(subCommand);
+                        }
                     }
                 }
+                return completion;
             }
+        } else {
+            return null;
         }
-        if (completer != null) {
-            completion.addAll(completer.onTabComplete(commandSender, command, s, strings));
-        }
-        return completion;*/
-        return DEFAULT_TAB_COMPLETER.onTabComplete(commandSender, command, s, strings);
     }
 }
