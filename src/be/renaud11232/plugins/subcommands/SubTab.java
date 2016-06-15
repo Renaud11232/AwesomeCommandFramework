cb3783239a9f4fef13b40e098f7353232ff4feb4
Renaud11232 <gaspardrenaud@hotmail.com>
2016-06-15 03:33:40 +0200

    added methods in Sub* classes and used them in constructors

--- src/be/renaud11232/plugins/subcommands/SubTab.java
+++ src/be/renaud11232/plugins/subcommands/SubTab.java
@@ -11,23 +11,9 @@ public class SubTab {
     private String[] aliases;
 
     public SubTab(String name, TabCompleter completer, String... aliases) {
-        if (name == null) {
-            throw new NullPointerException("Could not create a SubTab, name was null");
-        }
-        if (name.contains(" ")) {
-            throw new IllegalArgumentException("Could not create a SubTab, name contains a \" \" : \"" + name + "\"");
-        }
-        if (aliases == null) {
-            throw new NullPointerException("Could not create a SubTab, aliases was null");
-        }
-        for (String alias : aliases) {
-            if (alias.contains(" ")) {
-                throw new IllegalArgumentException("Could not create a SubTab, at least one of the given aliases contains a \" \" : \"" + alias + "\"");
-            }
-        }
-        this.name = name;
-        this.completer = completer == null ? ComplexTabCompleter.DEFAULT_COMPLETER : completer;
-        this.aliases = aliases;
+        setName(name);
+        setCompleter(completer);
+        setAliases(aliases);
     }
 
     public String getName() {
@@ -40,5 +26,31 @@ public class SubTab {
 
     public String[] getAliases() {
         return Arrays.copyOf(aliases, aliases.length);
+    }
+
+    public void setName(String name) {
+        if (name == null) {
+            throw new NullPointerException("Could not set the name, name was null");
+        }
+        if (name.contains(" ")) {
+            throw new IllegalArgumentException("Could not set the name, name contains a \" \" : \"" + name + "\"");
+        }
+        this.name = name;
+    }
+
+    public void setCompleter(TabCompleter completer) {
+        this.completer = completer == null ? ComplexTabCompleter.DEFAULT_COMPLETER : completer;
+    }
+
+    public void setAliases(String... aliases) {
+        if (aliases == null) {
+            throw new NullPointerException("Could not set the aliases, aliases was null");
+        }
+        for (String alias : aliases) {
+            if (alias.contains(" ")) {
+                throw new IllegalArgumentException("Could not set the aliases, at least one of the given aliases contains a \" \" : \"" + alias + "\"");
+            }
+        }
+        this.aliases = Arrays.copyOf(aliases, aliases.length);
     }
 }
