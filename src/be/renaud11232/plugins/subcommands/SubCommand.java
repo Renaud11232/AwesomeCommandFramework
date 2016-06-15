abb45192efbdfcd8a43f4d59fd64028858a1ec95
Renaud11232 <gaspardrenaud@hotmail.com>
2016-06-15 18:50:34 +0200

    added more doc

--- src/be/renaud11232/plugins/subcommands/SubCommand.java
+++ src/be/renaud11232/plugins/subcommands/SubCommand.java
@@ -4,12 +4,24 @@ import org.bukkit.command.CommandExecutor;
 
 import java.util.Arrays;
 
+/**
+ * Defines a {@link ComplexCommandExecutor}'s {@link SubCommand}.
+ */
 public class SubCommand {
 
     private String name;
     private CommandExecutor executor;
     private String[] aliases;
 
+    /**
+     * Construncts a new {@link SubCommand} with a given name, {@link CommandExecutor}, and some aliases.
+     *
+     * @param name     the name of the {@link SubCommand}.
+     * @param executor the {@link CommandExecutor} of the {@link SubCommand}, ComplexCommandExecutor.DEFAULT_EXECUTOR is used if the value is <code>null</code>.
+     * @param aliases  the aliases of the {@link SubCommand}.
+     * @throws NullPointerException     if the name, the aliases array or one of the aliases is null.
+     * @throws IllegalArgumentException if the name or one of the aliases contains a space.
+     */
     public SubCommand(String name, CommandExecutor executor, String... aliases) {
         if (name == null) {
             throw new NullPointerException("Could not create a SubCommand, name was null");
@@ -21,6 +33,9 @@ public class SubCommand {
             throw new NullPointerException("Could not create a SubCommand, aliases was null");
         }
         for (String alias : aliases) {
+            if (alias == null) {
+                throw new NullPointerException("Could not create a SubCommand, at least on of the given aliases was null");
+            }
             if (alias.contains(" ")) {
                 throw new IllegalArgumentException("Could not create a SubCommand, at least one of the given aliases contains a \" \" : \"" + alias + "\"");
             }
@@ -30,14 +45,29 @@ public class SubCommand {
         this.aliases = Arrays.copyOf(aliases, aliases.length);
     }
 
+    /**
+     * Gets the name of this {@link SubCommand}.
+     *
+     * @return the name of this {@link SubCommand}.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the {@link CommandExecutor} of this {@link SubCommand}.
+     *
+     * @return the {@link CommandExecutor} of this {@link SubCommand}.
+     */
     public CommandExecutor getExecutor() {
         return executor;
     }
 
+    /**
+     * Gets all the aliases of this {@link SubCommand}.
+     *
+     * @return a copy the aliases of this {@link SubCommand}.
+     */
     public String[] getAliases() {
         return Arrays.copyOf(aliases, aliases.length);
     }
