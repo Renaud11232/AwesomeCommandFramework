65c3d87eec1f79b1c1c60710295bf4b13712d640
Renaud Gaspard <gaspardrenaud@hotmail.com>
2018-06-25 17:38:01 +0200

    improved command generation and aliases support

--- src/main/java/be/renaud11232/plugins/awesomecommands/AwesomeCommand.java
+++ src/main/java/be/renaud11232/plugins/awesomecommands/AwesomeCommand.java
@@ -5,25 +5,25 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class AwesomeCommand extends Command {
 
     public static final CommandExecutor DEFAULT_EXECUTOR = (commandSender, command, s, strings) -> false;
+    public static final CommandExecutor NO_EXECUTOR = (commandSender, command, s, strings) -> true;
+
+    public static final TabCompleter DEFAULT_COMPLETER = (commandSender, command, s, strings) -> null;
+    public static final TabCompleter NO_COMPLETER = (commandSender, command, s, strings) -> Collections.emptyList();
 
     private Map<String, AwesomeCommand> subCommands;
-    private Map<String, AwesomeCommand> aliases;
     private CommandExecutor executor;
     private TabCompleter tabCompleter;
 
     public AwesomeCommand(String name) {
         super(name);
         subCommands = new HashMap<>();
-        aliases = new HashMap<>();
         executor = DEFAULT_EXECUTOR;
+        tabCompleter = DEFAULT_COMPLETER;
         usageMessage = "/<command>";
     }
 
@@ -50,13 +50,16 @@ public class AwesomeCommand extends Command {
 
     public void addSubCommand(AwesomeCommand awesomeCommand) {
         subCommands.put(awesomeCommand.getName(), awesomeCommand);
-        awesomeCommand.getAliases().forEach(alias -> aliases.put(alias, awesomeCommand));
+        awesomeCommand.getAliases()
+                .stream()
+                .filter(alias -> !subCommands.containsKey(alias))
+                .forEach(alias -> subCommands.put(alias, awesomeCommand));
     }
 
     @Override
     public boolean execute(CommandSender commandSender, String s, String[] strings) {
         if(testPermission(commandSender)) {
-            if(strings.length == 0 || (!subCommands.containsKey(strings[0]) && !aliases.containsKey(strings[0]))) {
+            if(strings.length == 0 || !subCommands.containsKey(strings[0])) {
                 if(!executor.onCommand(commandSender, this, s, strings)){
                     if(getUsage().length() > 0) {
                         Arrays.asList(getUsage().replace("<command>", s).split("(\r\n|\n)"))
@@ -64,8 +67,7 @@ public class AwesomeCommand extends Command {
                     }
                 }
             } else {
-                var subcommand = subCommands.containsKey(strings[0]) ? subCommands.get(strings[0]) : aliases.get(strings[0]);
-                subcommand.execute(commandSender, s + " " + strings[0], Arrays.copyOfRange(strings, 1, strings.length));
+                subCommands.get(strings[0]).execute(commandSender, s + " " + strings[0], Arrays.copyOfRange(strings, 1, strings.length));
             }
         }
         return true;
@@ -74,8 +76,9 @@ public class AwesomeCommand extends Command {
     @Override
     public List<String> tabComplete(CommandSender commandSender, String s, String[] strings) {
         if(testPermissionSilent(commandSender)) {
-
+            return null;
+        } else {
+            return NO_COMPLETER.onTabComplete(commandSender, this, s, strings);
         }
-        return null;
     }
 }
--- src/main/java/be/renaud11232/plugins/awesomecommands/AwesomePlugin.java
+++ src/main/java/be/renaud11232/plugins/awesomecommands/AwesomePlugin.java
@@ -1,11 +1,11 @@
 package be.renaud11232.plugins.awesomecommands;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class AwesomePlugin extends JavaPlugin {
 
@@ -13,22 +13,14 @@ public class AwesomePlugin extends JavaPlugin {
 
     public AwesomePlugin() {
         awesomeCommands = new HashMap<>();
-    }
-
-    @Override
-    public void onEnable() {
         getDescription().getCommands().forEach((key, value) -> {
             var awesomeCommand = parseCommand(key, value);
             awesomeCommands.put(key, awesomeCommand);
-            var command = getCommand(key);
-            command.setExecutor((commandSender, command1, s, strings) -> awesomeCommand.execute(commandSender, s, strings));
-            command.setTabCompleter((commandSender, command1, s, strings) -> awesomeCommand.tabComplete(commandSender, s, strings));
+            awesomeCommand.getAliases()
+                    .stream()
+                    .filter(alias -> !awesomeCommands.containsKey(alias))
+                    .forEach(alias -> awesomeCommands.put(alias, awesomeCommand));
         });
-    }
-
-    public AwesomeCommand getAwesomeCommand(String name){
-        var nameSplit = name.split(".", 2);
-        return nameSplit.length == 1 ? awesomeCommands.get(nameSplit[0]) : awesomeCommands.get(nameSplit[0]).getSubCommand(nameSplit[1]);
     }
 
     private AwesomeCommand parseCommand(String name, Map<String, Object> properties) {
@@ -53,5 +45,26 @@ public class AwesomePlugin extends JavaPlugin {
             ((Map<String, Map<String, Object>>) properties.get("subcommands")).forEach((key, value) -> awesomeCommand.addSubCommand(parseCommand(key, value)));
         }
         return awesomeCommand;
+    }
+
+    @Override
+    public final boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return getAwesomeCommand(command.getName()).execute(sender, label, args);
+    }
+
+    @Override
+    public final List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        return getAwesomeCommand(command.getName()).tabComplete(sender, alias, args);
+    }
+
+    @Override
+    @Deprecated
+    public PluginCommand getCommand(String name) {
+        throw new UnsupportedOperationException("This method has been replaced by the much more awesome 'getAwesomeCommand(String)' method");
+    }
+
+    public AwesomeCommand getAwesomeCommand(String name){
+        var nameSplit = name.split("\\.", 2);
+        return nameSplit.length == 1 ? awesomeCommands.get(nameSplit[0]) : awesomeCommands.get(nameSplit[0]).getSubCommand(nameSplit[1]);
     }
 }
