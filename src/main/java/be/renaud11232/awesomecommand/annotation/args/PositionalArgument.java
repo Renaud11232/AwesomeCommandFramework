7458fce3dc5050da9b63e4e3b41f04838efef5cf
Renaud Gaspard <gaspardrenaud@hotmail.com>
2022-12-01 09:44:15 +0100

    removed NamedArgument.java & PositionalArgument.java for now

--- src/main/java/be/renaud11232/awesomecommand/annotation/args/PositionalArgument.java
+++
@@ -1,9 +0,0 @@
-package be.renaud11232.awesomecommand.annotation.args;
-
-import java.lang.annotation.*;
-
-@Documented
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
-public @interface PositionalArgument {
-}
